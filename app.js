dc7e9703c2c252730af0a39ecce58321550f7d18
Aman Rawat <103962461+AmanRawat346@users.noreply.github.com>
2023-02-26 13:07:00 +0530

    add security

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { path, express, morgan, helmet, rateLimit, mongoSanitize, xss} = require('./dependencies');
+
 const express = require('express');
 
 const userRoutes = require('./routes/userRoutes');
@@ -8,6 +10,25 @@ const app = express();
 
 // Body Parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Serving static files 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set security for HTTP headers
+app.use(helmet());
+
+// Limt requests from same API
+const limiter = rateLimit({
+    max: 10000,  // no of attempt with given no of hours which is intialize in windowMs 
+    windowMs: 60 * 60 * 1000, // 1 hour -> within 1 hour there is 100 request possible with same ip
+    message: 'Too Many request from these IP, please try again in an hour!'
+});
+
+// Data sanitization against NoSql query injection
+app.use(mongoSanitize());
+
+// Data sanitization against xss
+app.use(xss());
 
 app.use('/user', userRoutes);
 app.use('/admin', adminRoutes);
---
+++ dependencies.js
@@ -0,0 +1,14 @@
+exports.path = require('path');
+
+exports.express = require('express');
+
+exports.morgan = require('morgan');
+
+exports.helmet = require('helmet');
+
+exports.rateLimit = require('express-rate-limit');
+
+exports.mongoSanitize = require('express-mongo-sanitize');
+
+exports.xss = require('xss-clean');
+
